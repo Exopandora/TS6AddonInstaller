4abd9f624c910a92fd6b54fb922198b8fd05dcab
Exopandora <mkonrad98@outlook.com>
2025-10-17 20:21:24 +0200

    Refactor version index

--- src/main/java/com/github/exopandora/ts6ai/model/VersionIndex.java
+++ src/main/java/com/github/exopandora/ts6ai/model/VersionIndex.java
@@ -1,33 +1,38 @@
 package com.github.exopandora.ts6ai.model;
 
-import static com.github.exopandora.ts6ai.util.Util.OBJECT_MAPPER;
-
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Map;
-import java.util.Optional;
-
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.github.exopandora.ts6ai.util.Lazy;
 import com.github.exopandora.ts6ai.util.OS;
 import com.github.exopandora.ts6ai.util.Util;
 import com.vdurmont.semver4j.Semver;
 import com.vdurmont.semver4j.Semver.SemverType;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static com.github.exopandora.ts6ai.util.Util.OBJECT_MAPPER;
+
 public class VersionIndex {
-	private final Map<String, String> windows;
-	private final Map<String, String> linux;
-	private final Map<String, String> macos;
+	private final Lazy<Map<String, String>> windows;
+	private final Lazy<Map<String, String>> linux;
+	private final Lazy<Map<String, String>> macos;
 	
 	public VersionIndex(
-		@JsonProperty("windows") Map<String, String> windows,
-		@JsonProperty("linux") Map<String, String> linux,
-		@JsonProperty("macos") Map<String, String> macos
+		@JsonProperty("windows") Map<String, Set<String>> windows,
+		@JsonProperty("linux") Map<String, Set<String>> linux,
+		@JsonProperty("macos") Map<String, Set<String>> macos
 	) {
-		this.windows = windows;
-		this.linux = linux;
-		this.macos = macos;
+		this.windows = new Lazy<Map<String, String>>(() -> createLookup(windows));
+		this.linux = new Lazy<Map<String, String>>(() -> createLookup(linux));
+		this.macos = new Lazy<Map<String, String>>(() -> createLookup(macos));
 	}
 	
 	@JsonIgnore
@@ -35,16 +40,22 @@ public class VersionIndex {
 		String result = null;
 		switch(os) {
 			case WINDOWS:
-				result = this.windows.get(md5);
+				result = this.windows.get().get(md5);
 				break;
 			case LINUX:
-				result = this.linux.get(md5);
+				result = this.linux.get().get(md5);
 				break;
 			case MAC_OS:
-				result = this.macos.get(md5);
+				result = this.macos.get().get(md5);
 				break;
 		}
 		return Optional.ofNullable(result);
+	}
+	
+	private static Map<String, String> createLookup(Map<String, Set<String>> versionToHashes) {
+		return versionToHashes.entrySet().stream()
+			.flatMap(entry -> entry.getValue().stream().map(hash -> new SimpleEntry<String, String>(hash, entry.getKey())))
+			.collect(Collectors.toMap(Entry::getKey, Entry::getValue));
 	}
 	
 	public static Semver findTeamSpeakVersion(String installDir, OS os) throws IOException, IllegalStateException {
