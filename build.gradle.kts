d3497d3b0db31f1afd4825d1f49bafd5e42736f1
Exopandora <mkonrad98@outlook.com>
2026-02-20 20:53:28 +0100

    Update to dom4j 2.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ dependencies {
 	implementation("commons-cli:commons-cli:1.11.0")
 	implementation("com.fasterxml.jackson.core:jackson-databind:2.20.0")
 	implementation("com.vdurmont:semver4j:3.1.0")
-	implementation("org.dom4j:dom4j:2.1.5")
+	implementation("org.dom4j:dom4j:2.2.0")
 }
 
 tasks.withType<JavaExec> {
