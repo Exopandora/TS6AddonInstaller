ace78bfcc5e58e350b1b7c731d548f941f0a4f9b
Exopandora <mkonrad98@outlook.com>
2026-05-27 21:39:35 +0200

    Update to build-health plugin 3.13.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.name = "TS6AddonInstaller"
 
 plugins {
-    id("com.autonomousapps.build-health") version "3.5.1"
+    id("com.autonomousapps.build-health") version "3.13.0"
     id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
